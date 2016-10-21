8db7cbf3c2b4b75a63511becb19ecde1b80cda90
Ari Lerner <arilerner@mac.com>
2016-10-21 11:23:56 +0100

    Updates

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,67 +1,92 @@
 import React from 'react';
-var ReactDOM = require('react-dom');
-import {GoogleApiWrapper} from 'google-maps-react'
+import {Map, GoogleApiWrapper} from 'google-maps-react'
 
 /*
 Google Maps support for the app
 */
 
 export class Container extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.mapConfig = this._getMapConfig(this.props.google);
+  }
+
+  componentWillReceiveProps(newProps) {
+    if (newProps.google !== this.props.google) {
+      this.mapConfig = this._getMapConfig(newProps.google);
+    }
+  }
+
+  _getMapConfig(google) {
+    let zoom = 14;
+    let lat = 39.774929;
+    let lng = -112.419416;
+    const center = { lat, lng };
+    return Object.assign({}, {
+      initialCenter: center,
+      zoom: zoom,
+      google: google
+    });
+  }
+
   render() {
     const style = {
       width: '100vw',
       height: '100vh'
-    }
+    };
+
     return (
       <div style={style}>
-        <Map google={this.props.google} />
+        <Map {...this.mapConfig} />
       </div>
     )
   }
 }
 
-export class Map extends React.Component {
-  componentDidMount() {
-    this.loadMap();
-  }
+// export class Map extends React.Component {
+//   componentDidMount() {
+//     this.loadMap();
+//   }
 
-  componentDidUpdate(prevProps, prevState) {
-    if (prevProps.google !== this.props.google) {
-      this.loadMap();
-    }
-  }
+//   componentDidUpdate(prevProps, prevState) {
+//     if (prevProps.google !== this.props.google) {
+//       this.loadMap();
+//     }
+//   }
 
-  loadMap() {
-    if (this.props && this.props.google) {
-      // google is available
-      const {google} = this.props;
-      const maps = google.maps;
+//   loadMap() {
+//     console.log('loadMap', this.props)
+//     if (this.props && this.props.google) {
+//       // google is available
+//       const {google} = this.props;
+//       const maps = google.maps;
 
-      const mapRef = this.refs.map;
-      console.log(mapRef);
-      const node = ReactDOM.findDOMNode(mapRef);
-      console.log(node);
+//       const mapRef = this.refs.map;
+//       const node = ReactDOM.findDOMNode(mapRef);
 
-      let zoom = 14;
-      let lat = 37.774929;
-      let lng = -122.419416;
-      const center = new maps.LatLng(lat, lng);
-      const mapConfig = Object.assign({}, {
-        center: center,
-        zoom: zoom
-      })
-      this.map = new maps.Map(node, mapConfig);
-    }
-  }
+//       let zoom = 14;
+//       let lat = 37.774929;
+//       let lng = -122.419416;
+//       const center = new maps.LatLng(lat, lng);
+//       const mapConfig = Object.assign({}, {
+//         center: center,
+//         zoom: zoom
+//       })
+//       this.map = new maps.Map(node, mapConfig);
 
-  render() {
-    return (
-      <div ref='map'>
-        Loading map...
-      </div>
-    )
-  }
-}
+//       console.log('map ->', this.map);
+//     }
+//   }
+
+//   render() {
+//     return (
+//       <div ref='map'>
+//         Loading map...
+//       </div>
+//     )
+//   }
+// }
 
 export default GoogleApiWrapper({
   apiKey: "AIzaSyCM2-HdiEngSELW0kkSVRi__BpNCV5MG-Y"
